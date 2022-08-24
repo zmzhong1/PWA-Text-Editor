8ce05ca4f5fc20a6c63d9764901408e96eed18f5
zmzhong1 <zhongzm8e@gmail.com>
2022-08-23 20:01:11 -0700

    offline update

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,23 +14,22 @@ const initdb = async () =>
 
 // put database method
 export const putDb = async (content) => {
-  console.log("PUT to the database");
+  console.log("Seeking DB data to update.");
   const jateDB = await openDB("jate", 1);
   const jateReadWrite = jateDB.transaction("jate", "readwrite");
-  const jateStore = jateReadWrite.objectStore("jate");
-  const request = jateStore.put({ id: 1, value: DBContent });
-  const result = await request;
-  console.log("🚀 - data saved to the database", result);
+  const jateObject = jateReadWrite.objectStore("jate");
+  const request = jateObject.put({ id: 1, value: content });
+  const response = await request;
+  console.log(`Did it work? ${response}`);
 };
 // get all database method
 export const getDb = async () => {
-  console.log("GET from the database");
+  console.log("Seeking DB data to update.");
   const jateDB = await openDB("jate", 1);
-  const jateReadWrite = jateDB.transaction("jate", "readonly");
-  const jateStore = jateReadWrite.objectStore("jate");
-  const request = jateStore.getAll(1);
+  const jateRead = jateDB.transaction("jate", "readonly");
+  const jateObject = jateRead.objectStore("jate");
+  const request = jateObject.getAll(1);
   const result = await request;
-  console.log("result.value", result);
   return result;
 };
 
